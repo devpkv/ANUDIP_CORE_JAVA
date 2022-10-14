c17f23ee90688c00d371ef0eee35d0171467dee6
Pankaj Kumar <54941511+devpkv@users.noreply.github.com>
2022-10-14 11:13:42 +0530

    Core java practice 14 oct 2022

---
+++ 12_Oct/Info.java
@@ -0,0 +1,10 @@
+class Info{
+
+	public static void main(String arg[])
+	{
+		System.out.println("Name:"+ arg[0]+" "+arg[1]);
+		System.out.println("Age:"+ arg[2]);
+		System.out.println("Course:"+ arg[3]);
+		System.out.println("Score:"+ arg[4]);
+	}
+}
---
+++ 14_Oct/DataType.java
@@ -0,0 +1,18 @@
+class DataType { 
+	public static void main(String[] args){ 
+		String name = "Pankaj Kumar";
+		char gender = 'M';
+		int age = 24;
+		float marks1 = 300.6f;
+		double marks2 = 200.5;
+		boolean pass = true;
+
+		System.out.println("name : "+name);
+		System.out.println("gender : "+gender);
+		System.out.println("age : "+age);
+		System.out.println("marks1 : "+marks1);
+		System.out.println("marks2 : "+marks2);
+		System.out.println("pass : "+pass);
+	} 
+} 
+ 
