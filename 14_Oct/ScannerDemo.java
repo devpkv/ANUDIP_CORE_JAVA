777a32e35ed0a490126a07fda754fedfa37f683f
Pankaj Kumar <54941511+devpkv@users.noreply.github.com>
2022-10-14 12:20:14 +0530

    Scanner Class Practice

---
+++ 14_Oct/ScannerDemo.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class ScannerDemo { 
+	public static void main(String[] args){ 
+		 
+		 Scanner sc = new Scanner(System.in);
+
+		 System.out.println("Enter Some String");
+		 String str;
+		 str = sc.nextLine();
+
+		 System.out.println("Entered String = "+ str);
+	} 
+} 
+ 
